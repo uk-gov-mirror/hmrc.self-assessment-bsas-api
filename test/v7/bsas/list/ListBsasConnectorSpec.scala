30ce23e2dbcd0b92a271413bb998f3568061fa0c
muhammadmulla1 <muhammad_mulla@icloud.com>
2026-08-10 14:41:15 +0100

    MTDSA-36976: Implement HIP migration for API #1898 3

--- test/v7/bsas/list/ListBsasConnectorSpec.scala
+++ test/v7/bsas/list/ListBsasConnectorSpec.scala
@@ -104,7 +104,7 @@ class ListBsasConnectorSpec extends ConnectorSpec {
     protected def downstreamQueryParams: Seq[(String, String)]
 
     ((() => mockAppConfig.isHipMigration1898Enabled)).expects().returning(false).anyNumberOfTimes()
-    
+
     protected val request: ListBsasRequestData =
       Def2_ListBsasRequestData(nino, taxYear, Some(BusinessId(incomeSourceId)), Some(incomeSourceType))
 
